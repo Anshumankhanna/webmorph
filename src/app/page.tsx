8ea94b5b2431e3bed6999759170deea6c4c1354b
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-04-16 15:17:30 +0530

    Added try-catch.ts util

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 
 import requestBot, { BotOutput } from "@/actions/request-bot";
 import asyncTryCatch from "@/utils/async-try-catch";
-import { useState } from "react";
+import tryCatch from "@/utils/try-catch";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 	const [, setData] = useState<BotOutput | null>(null);
--- src/utils/try-catch.ts
+++ src/utils/try-catch.ts
@@ -1,10 +1,12 @@
 import { Result } from "@/types/result";
 
-export default function tryCatch<T>(func: () => T): Result<T> {
+export default function tryCatch<T, E = Error>(
+	func: () => T
+): Result<T, E> {
 	try {
 		const data = func();
 		return { data, error: null };
 	} catch (error) {
-		return { data: null, error: error as Error };
+		return { data: null, error: error as E };
 	}
 }
