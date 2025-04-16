2e586e5736305ab83b136ff9a1b20a3ef55f4eee
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-04-16 15:09:37 +0530

    Fixed indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,38 @@ import asyncTryCatch from "@/utils/async-try-catch";
 import { useState } from "react";
 
 export default function Home() {
-  const [, setData] = useState<BotOutput | null>(null);
-  const [input, setInput] = useState("");
-  const [loading, setLoading] = useState(false);
+	const [, setData] = useState<BotOutput | null>(null);
+	const [input, setInput] = useState("");
+	const [loading, setLoading] = useState(false);
 
-  const request = async () => {
-    setLoading(true);
+	const request = async () => {
+		setLoading(true);
 
-    const botOutput = await asyncTryCatch(requestBot(input));
+		const botOutput = await asyncTryCatch(requestBot(input));
 
-    setLoading(false);
+		setLoading(false);
 
-    if (botOutput.error !== null) {
-      return;
-    }
+		if (botOutput.error !== null) {
+			return;
+		}
 
-    setData(botOutput.data);
-  }
+		setData(botOutput.data);
+	};
 
-  return (
-    <div className="flex gap-2 p-2">
-      <input className="border rounded-lg" value={input} onChange={(event) => setInput(event.target.value)} />
-      <button className="rounded-lg bg-blue-500 p-2 text-white disabled:bg-gray-500" onClick={request} disabled={loading}>Click to process</button>
-    </div>
-  );
+	return (
+		<div className="flex gap-2 p-2">
+			<input
+				className="border rounded-lg"
+				value={input}
+				onChange={(event) => setInput(event.target.value)}
+			/>
+			<button
+				className="rounded-lg bg-blue-500 p-2 text-white disabled:bg-gray-500"
+				onClick={request}
+				disabled={loading}
+			>
+				Click to process
+			</button>
+		</div>
+	);
 }
